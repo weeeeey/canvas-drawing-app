266f361a876e232269fa34542f794c589ca70fca
zerojin <qser155@naver.com>
2023-09-13 12:30:04 +0900

    drwaing canvas

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import { FC, useState } from 'react';
 import { useDraw } from '../hooks/useDraw';
 import { ChromePicker } from 'react-color';
 
-interface pageProps {}
-
-const Page: FC<pageProps> = ({}) => {
-    const [color, setColor] = useState<string>('#000000');
+const Page = ({}) => {
+    const [color, setColor] = useState<string>('#000');
     const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
 
     function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
@@ -32,12 +30,7 @@ const Page: FC<pageProps> = ({}) => {
     return (
         <div className="w-screen h-screen bg-white flex justify-center items-center">
             <div className="flex flex-col gap-10 pr-10">
-                <ChromePicker
-                    onChange={(e) => {
-                        setColor(e.hex);
-                        console.log(color);
-                    }}
-                />
+                <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
                 <button
                     type="button"
                     className="p-2 rounded-md border border-black"
